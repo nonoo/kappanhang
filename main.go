cbfe35a7de1bd3cff3f742cea3d8c9bf84d05c4e
Akos Marton <akos.marton@gmail.com>
2020-10-20 09:47:42 +0300

    Add audio pipes

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/akosmarton/papipes"
 	"github.com/nonoo/kappanhang/log"
 )
 
 var streams struct {
 	control controlStream
 	audio   audioStream
+}
+
+var audioPipes struct {
+	source papipes.Source
+	sink   papipes.Sink
 }
 
 func exit(err error) {
@@ -20,6 +26,18 @@ func exit(err error) {
 
 	streams.audio.sendDisconnect()
 	streams.control.sendDisconnect()
+
+	if audioPipes.source.IsOpen() {
+		if err := audioPipes.source.Close(); err != nil {
+			log.Error(err)
+		}
+	}
+
+	if audioPipes.sink.IsOpen() {
+		if err := audioPipes.sink.Close(); err != nil {
+			log.Error(err)
+		}
+	}
 
 	log.Print("exiting")
 	if err == nil {
@@ -42,6 +60,31 @@ func main() {
 	log.Init()
 	log.Print("kappanhang by Norbert Varga HA2NON https://github.com/nonoo/kappanhang")
 	parseArgs()
+
+	audioPipes.source.Name = "kappanhang"
+	audioPipes.source.Filename = "/tmp/kappanhang.source"
+	audioPipes.source.Rate = 48000
+	audioPipes.source.Format = "s16le"
+	audioPipes.source.Channels = 1
+	audioPipes.source.SetProperty("device.buffering.buffer_size", 1920*5)
+	audioPipes.source.SetProperty("device.description", "kappanhang input")
+
+	audioPipes.sink.Name = "kappanhang"
+	audioPipes.sink.Filename = "/tmp/kappanhang.sink"
+	audioPipes.sink.Rate = 48000
+	audioPipes.sink.Format = "s16le"
+	audioPipes.sink.Channels = 1
+	audioPipes.sink.SetProperty("device.buffering.buffer_size", 1920*5)
+	audioPipes.sink.SetProperty("device.description", "kappanhang output")
+
+	if err := audioPipes.source.Open(); err != nil {
+		exit(err)
+	}
+
+	if err := audioPipes.sink.Open(); err != nil {
+		exit(err)
+	}
+
 	setupCloseHandler()
 
 	streams.audio.init()
