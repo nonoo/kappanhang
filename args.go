dfe09051e24cfd3e7504bf146bb6a1c1e82e060c
Nonoo <nonoo@nonoo.hu>
2020-10-25 22:23:37 +0100

    Clarify cmdline argument's description

--- args.go
+++ args.go
@@ -13,7 +13,7 @@ var enableSerialDevice bool
 func parseArgs() {
 	h := getopt.BoolLong("help", 'h', "display help")
 	a := getopt.StringLong("address", 'a', "IC-705", "Connect to address")
-	t := getopt.Uint16Long("serial-tcp-port", 'p', 4533, "Expose radio's serial port as TCP port for rigctl")
+	t := getopt.Uint16Long("serial-tcp-port", 'p', 4533, "Expose radio's serial port on this TCP port")
 	s := getopt.BoolLong("enable-serial-device", 's', "Expose radio's serial port as a virtual serial port")
 
 	getopt.Parse()
