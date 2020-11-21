c5a6742260698b58624929d05d32e9375c4db367
Nonoo <nonoo@nonoo.hu>
2020-11-21 11:02:52 +0100

    Increase serial stream seqbuf length

--- serialstream.go
+++ serialstream.go
@@ -7,7 +7,7 @@ import (
 )
 
 const maxSerialFrameLength = 80 // Max. frame length according to Hamlib.
-const serialRxSeqBufLength = 200 * time.Millisecond
+const serialRxSeqBufLength = 300 * time.Millisecond
 
 type serialStream struct {
 	common streamCommon
